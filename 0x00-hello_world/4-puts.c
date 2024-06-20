64d54510fc1b84cd7577b3519ae31d3a810a04f1
LinMihigo <linmihigo@gmail.com>
2024-06-20 11:29:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - A C program that prints a line
+  * Description: A C program that prints the exact string -
+  * "Programming is like building a multilingual puzzle, followed by a new line
+  * Return - return the string "Programming is like building
+  * a multilingual puzzle
+  */
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
