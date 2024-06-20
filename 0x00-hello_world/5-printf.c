0890b0af85ca12d40dd6befeada62253cba258d4
LinMihigo <linmihigo@gmail.com>
2024-06-20 11:44:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - A C program that prints a string using printf
+  * Description: A C program that prints exactly with proper grammar,
+  * but the outcome is a piece of art,, followed by a new line.
+  * Return: the string "with proper grammar, but the outcome is
+  * a piece of art,"
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
