a872516d0bd7b34c99c68df1ac4f6aa4551dbe11
LinMihigo <linmihigo@gmail.com>
2024-06-20 16:41:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 /**
   * main - A function that prints a string
   * Description: A program that prints the string "and that piece
@@ -8,7 +10,19 @@
   * 2015-10-19"
   */
 int main(void)
-{	
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+{
+	char *a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t bytes_written = write(2, a, strlen(a));
+
+	if (bytes_written == -1)
+	{
+		return (1);
+	}
+
+	if ((size_t)bytes_written != strlen(a))
+	{
+		return (1);
+	}
+
 	return (1);
 }
