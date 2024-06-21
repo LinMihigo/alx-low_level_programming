3b2c180130a968978624cc07a828f95a55eeacfa
LinMihigo <linmihigo@gmail.com>
2024-06-21 16:55:17 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - a function
+  *
+  * Description: formality
+  *
+  * Return: a string
+  */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
