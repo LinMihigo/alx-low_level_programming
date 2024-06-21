6a7e6ad53f849309af7730ed7c63ac59e18bbc1d
LinMihigo <linmihigo@gmail.com>
2024-06-21 14:51:54 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+  * main - A function returning a string
+  *
+  * Description: A function that returns a string that contains the last
+  * digit of n and indicates if it is > 5, < 6 or 0
+  *
+  * Return: A string stating if n is > 5, < 6 or =  0
+  */
+int main(void)
+{
+	int n;
+	int z;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	z = n % 10;
+
+	if (z > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, z);
+	else if (z < 6)
+		printf("Last digit of %d is %d and is less than 6 and 0\n", n, z);
+	else
+		printf("Last digit of %d is %d and is 0\n", n, z);
+
+	return (0);
+}
