ea7774a2f00765c2c47494d8335b149466914126
LinMihigo <linmihigo@gmail.com>
2024-06-23 11:21:06 -0700

    102-print_comb5.c (10th fix)

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
+
 /**
-* main - entry point
-* printUniquePairs - function to print unique pairs
+* if_function - function to reduce indentation level
 *
-* Description: print unique combos of two two-digit numbers
+* @i: First digit
+* @j: Second digit
+* @k: Third digit
+* @l: Fourth digit
 *
-* Return: 0 (Success)
+* Prints a unique pair of two two-digit numbers.
 */
-void printUniquePairs(void)
+void if_function(int i, int j, int k, int l)
+{
+	if ((i != k || j != l) && (i < k || (i == k && l > j)))
+	{
+		putchar('0' + i);
+		putchar('0' + j);
+		putchar(' ');
+		putchar('0' + k);
+		putchar('0' + l);
+		if (!(i == 9 && j == 8 && k == 9 && l == 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+
+/**
+* print_unique_pairs - Function to print unique pairs
+*
+* Description: Print all unique combinations of two two-digit numbers.
+*
+* Return: 0
+*/
+void print_unique_pairs(void)
 {
 	int i = 0;
 
@@ -25,19 +52,7 @@ void printUniquePairs(void)
 
 				while (l < 10)
 				{
-					if ((i != k || j != l) && (i < k || (i == k && l > j)))
-					{
-						putchar('0' + i);
-						putchar('0' + j);
-						putchar(' ');
-						putchar('0' + k);
-						putchar('0' + l);
-						if (!(i == 9 && j == 8 && k == 9 && l == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					if_function(i, j, k, l);
 					l++;
 				}
 				k++;
@@ -49,8 +64,15 @@ void printUniquePairs(void)
 	putchar('\n');
 }
 
+/**
+* main - Entry point
+*
+* Description: Print unique combinations of two two-digit numbers.
+*
+* Return: 0 (Success)
+*/
 int main(void)
 {
-	printUniquePairs();
+	print_unique_pairs();
 	return (0);
 }
