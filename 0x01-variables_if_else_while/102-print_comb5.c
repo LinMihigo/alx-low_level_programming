34f9ee2414ff1ffeabadb3a674b8a9383827e3a6
LinMihigo <linmihigo@gmail.com>
2024-06-23 10:41:59 -0700

    102-print_comb5.c (9th fix)

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,44 +1,56 @@
 #include <stdio.h>
 /**
 * main - entry point
+* printUniquePairs - function to print unique pairs
 *
 * Description: print unique combos of two two-digit numbers
 *
 * Return: 0 (Success)
 */
-int main(void)
+void printUniquePairs(void)
 {
-	int i, j, k, l;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
-		for (j = 0; i < 10; j++)
-		{
-			for (k = 0; k < 10; k++)
-			{
-				for (l = 0; l < 10; l++)
-				{
-					if (i != k || j != l)
-					{
-						if (i < k || (i == k && l > j))
-						{
-							putchar('0' + i);
-							putchar('0' + j);
-							putchar(' ');
-							putchar('0' + k);
-							putchar('0' + l);
+		int j = 0;
 
-							if (i < 9 || j < 8 || k < 9 || l < 9)
-							{
-								putchar(',');
-								putchar(' ');
-							}
+		while (j < 10)
+		{
+			int k = 0;
+
+			while (k < 10)
+			{
+				int l = 0;
+
+				while (l < 10)
+				{
+					if ((i != k || j != l) && (i < k || (i == k && l > j)))
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(' ');
+						putchar('0' + k);
+						putchar('0' + l);
+						if (!(i == 9 && j == 8 && k == 9 && l == 9))
+						{
+							putchar(',');
+							putchar(' ');
 						}
 					}
+					l++;
 				}
+				k++;
 			}
+			j++;
 		}
+		i++;
 	}
 	putchar('\n');
+}
+
+int main(void)
+{
+	printUniquePairs();
 	return (0);
 }
