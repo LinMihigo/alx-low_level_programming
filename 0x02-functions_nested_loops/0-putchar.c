3b991877c613428c51f2435a954b3dd21c171495
LinMihigo <linmihigo@gmail.com>
2024-06-25 10:22:10 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* _putchar - a function
+* Description - Prints '_putchar' followed by a new line
+* Return: 0
+*/
+int _putchar(void)
+{
+	putchar('_putchar');
+	putchar('\n');
+}
