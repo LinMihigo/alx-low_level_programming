d4f028deb5f57157dbdf17345613499f2c028caf
LinMihigo <linmihigo@gmail.com>
2024-06-25 13:50:53 -0700

    1-alphabet.c (1st fix)

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,15 @@
 * Description: Prints the alphabet
 * Return: 0
 */
-void print_alphabet(void)
+int print_alphabet(void)
 {
 	int c;
 	int i;
 
-	for (i = 0; i < 26, i++)
+	for (i = 0; i < 26; i++)
 	{
 		c = 'a' + i;
-		_putchar(c)
+		_putchar(c);
 	}
 	_putchar('\n');
 	return (0);
