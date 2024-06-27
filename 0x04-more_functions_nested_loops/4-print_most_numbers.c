6b2f065faf6902c69e016d3e3e573f4a634498e0
LinMihigo <linmihigo@gmail.com>
2024-06-27 10:27:34 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_most_numbers - func
+* Return: 0
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int n = '0' + i;
+
+		if (n != 2 && n != 4)
+		{
+			_putchar(n);
+		}
+	}
+	_putchar('\n');
+}
