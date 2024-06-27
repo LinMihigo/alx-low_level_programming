ad000bd063bc20db9561654c64ddec6506b46c9c
LinMihigo <linmihigo@gmail.com>
2024-06-27 14:36:28 -0700

    0-isupper.c (3rd fix)

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,18 +6,9 @@
 */
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 0; i <= 26; i++)
+	if (isupper(c))
 	{
-		if (c == 'A' + i)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
 	}
 	return (0);
 }
