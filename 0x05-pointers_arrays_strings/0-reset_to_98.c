993d0feeeffb744a7164d04cb2cd6a569b244a2d
LinMihigo <linmihigo@gmail.com>
2024-07-02 10:28:11 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - func to update a pointer's value
+* @n: pointer param
+* Return: 0
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Rand al Thor
