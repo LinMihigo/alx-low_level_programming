6c68b18299b8c6263b33cba748286b894132db94
LinMihigo <linmihigo@gmail.com>
2024-07-04 13:58:50 -0700

    6-cap_string.c (3rd fix)

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,10 +18,10 @@ char *cap_string(char *str)
 			}
 
 			if (str[i - 1] == 32 || str[i - 1] == 9 || str[i - 1] == 10 ||
-				str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
-				str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
-				str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123 ||
-				str[i - 1] == 124)
+			str[i - 1] == 44 || str[i - 1] == 59 || str[i - 1] == 46 ||
+			str[i - 1] == 33 || str[i - 1] == 63 || str[i - 1] == 34 ||
+			str[i - 1] == 40 || str[i - 1] == 41 || str[i - 1] == 123 ||
+			str[i - 1] == 124)
 			{
 				str[i] = str[i] - 32;
 			}
