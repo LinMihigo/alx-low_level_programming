9f94aee8bae4af33f05522324da4c3fbb77d7d08
LinMihigo <linmihigo@gmail.com>
2024-07-08 13:51:49 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strspn - gets the length of a prefix substring
+* @s: pointer to string to be checked
+* @accept: characters to look up
+* Return: no. of bytes
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		for (i = 0; s[i] == accept[j]; i++)
+		{
+			if (i > j)
+			{
+				j = i;
+			}
+		}
+	}
+
+	return (j + 1);
+}
