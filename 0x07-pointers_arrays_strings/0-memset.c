96d8732822f855433a7a22719058f1c1dff5d116
LinMihigo <linmihigo@gmail.com>
2024-07-08 04:06:53 -0700

    0-memset.c (1st fix)

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _memset - copies memory area
+* @s: pointer to memory to be altered
+* @b: char to use to alter
+* @n: number of times to alter
+* Return: char
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+}
