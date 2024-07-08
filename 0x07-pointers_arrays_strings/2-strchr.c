3a9650f405ab940d248d125f98cc73ec2a0a3430
LinMihigo <linmihigo@gmail.com>
2024-07-08 10:17:06 -0700

    2-strchr.c (3rd fix)

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	if (*s == '\0')
-		return (s);
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 			return (s + i);
 	}
 
-	return (NULL);
+	return ('\0');
 }
