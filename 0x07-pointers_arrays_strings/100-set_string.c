674cda7ebc77c211641dca52f687e3a8dcb07452
LinMihigo <linmihigo@gmail.com>
2024-07-09 11:11:36 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* set_string - prints the sum of the two diagonals
+* of a square matrix of integers
+* @s: square matrix of which we print the sum of diagonals
+* @to: size of the matrix
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
