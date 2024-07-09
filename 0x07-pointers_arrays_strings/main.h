12477c49990156b7905370f746b7935d7e09bcd9
LinMihigo <linmihigo@gmail.com>
2024-07-09 10:48:31 -0700

    Updated main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
