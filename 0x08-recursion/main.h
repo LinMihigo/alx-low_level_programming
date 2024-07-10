aa4c3be88cff1c31d295c0a99ed9ade0e7f11fe1
LinMihigo <linmihigo@gmail.com>
2024-07-10 11:01:57 -0700

    2-strlen_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,9 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-#include <string.h>
-
 void _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
 
 #endif
