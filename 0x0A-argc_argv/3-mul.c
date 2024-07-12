05d08ca0f4ee2e417a748b7329e2a4299c405649
LinMihigo <linmihigo@gmail.com>
2024-07-12 13:45:11 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+* _atoi - func
+*	including the terminating null byte (\0)
+* @s: param
+* Return: An integer
+*/
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int z, x, y;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = _atoi(argv[1]);
+	y = _atoi(argv[2]);
+	z = x * y;
+
+	printf("%d\n", z);
+
+	return (0);
+}
