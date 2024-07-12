834815eca295983f4f932196035a061e91bc5ac4
LinMihigo <linmihigo@gmail.com>
2024-07-12 13:10:41 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints the name of the program
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
