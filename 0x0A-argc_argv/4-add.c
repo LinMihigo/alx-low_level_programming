cc16bc909025cb60cedabd13218f420222eec9db
LinMihigo <linmihigo@gmail.com>
2024-07-13 03:01:04 -0700

    4-add.c (1st fix)

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,31 @@
 #include "main.h"
 /**
+ * _atoi - func
+ *	including the terminating null byte (\0)
+ * @s: param
+ * Return: An integer
+*/
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+		else if (*s != '\0' && (*s < '0' || *s > '9'))
+		{
+			num = 0;
+			break;
+		}
+	} while (*s++);
+
+	return (num * sign);
+}
+
+/**
 * main - adds +ve nums
 * @argc: argument count
 * @argv: argument arr of char
@@ -11,14 +37,14 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		if (_atoi(argv[i]) == 0)
 		{
 			printf("%s\n", "Error");
 			return (1);
 		}
 		else
 		{
-			z += atoi(argv[i]);
+			z += _atoi(argv[i]);
 		}
 	}
 	printf("%d\n", z);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,7 @@
 #define HEADER_H
 
 #include <stdio.h>
-#include <stdlib.h>
+
+int _atoi(char *s);
 
 #endif
