08971b15837b8de636e4fb6c6e2854b315da4093
LinMihigo <linmihigo@gmail.com>
2024-07-15 08:55:36 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* _strlen - returns length of a string
+* @str: string
+* Return: int
+*/
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+* _strdup - return a pointer to a newly allocated space in memory
+* @str: string copied to the newly allocated memory space
+* Return: pointer to char
+*/
+char *_strdup(char *str)
+{
+	int j = 0;
+	int n = _strlen(str) + 1; /* Adding 1 coz _strlen checks till \0 */
+	char *ar = malloc(sizeof(char) * n);
+
+	while (j < n && *(str + j) != '\0')
+	{
+		ar[j] = str[j];
+		j++;
+	}
+	ar[n - 1] = '\0';
+
+	return (ar);
+}
