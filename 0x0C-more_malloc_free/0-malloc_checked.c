4d18b60c5551add0080b2710f4e58c8d7bbbcd46
LinMihigo <linmihigo@gmail.com>
2024-07-18 11:07:01 -0700

    0-malloc_checked.c 2nd fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,12 +6,12 @@
 */
 void *malloc_checked(unsigned int b)
 {
-	if (malloc(b) == NULL)
-	{
+	void *pt;
+
+	pt = malloc(b);
+
+	if (pt == NULL)
 		exit(98);
-	}
-	else
-	{
-		return (malloc(b));
-	}
+
+	return (pt);
 }
