6b267881721eb54a212061bc77dadc99cea015c8
LinMihigo <linmihigo@gmail.com>
2024-07-22 10:20:09 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Siuan Sanche
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - a dog's details
+* @name: first member
+* @age: second member
+* @owner: 3rd member
+* Description: contains a dog's name, age, and owner name.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
