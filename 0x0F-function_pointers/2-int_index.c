85724025259d62bbe3d753d67786bbb2d6404edd
LinMihigo <linmihigo@gmail.com>
2024-07-24 11:50:27 -0700

    2-int_index.c (1st fix)

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,20 +8,15 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, n = 0;
-
-	if (!array || !cmp)
-		return;
+	int i;
 
 	for (i = 0; i < size; i++)
 	{
 		if ((*cmp)(array[i]) != 0)
 		{
-			n++;
 			return (i);
 		}
 	}
 
-	if (size <= 0 || n == 0)
-		return (-1);
+	return (-1);
 }
