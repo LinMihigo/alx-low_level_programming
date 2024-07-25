63cbdf2c67f5114924425e7fe0896ca866d0b338
LinMihigo <linmihigo@gmail.com>
2024-07-25 01:03:18 -0700

    1-array_iterator.c (After 1st deadline 1st fix) - I am getting an error that gcc:error:226/959/main_0.c: No such file or directory

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,13 +8,13 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
 
 	if (!action || !array)
 		return;
 
 	for (i = 0; i < size; i++)
 	{
-		(*action)(array[i]);
+		action(array[i]);
 	}
 }
