079c5989250e821eb5313a1ca6e26cae5dec4e8a
LinMihigo <linmihigo@gmail.com>
2024-07-25 01:40:31 -0700

    Revert "15th check fix"
    This reverts commit 6d0456058bdd25cfbc86d0ff37e9a5ded5ed312a.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,7 +7,8 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
+	int res, num1 = atoi(argv[1]), num2 = atoi(argv[3]);
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
@@ -15,7 +16,8 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (get_op_func(argv[2]) == NULL && strlen(argv[2]) != 1)
+	f = get_op_func(argv[2]);
+	if (!f)
 	{
 		printf("%s\n", "Error");
 		exit(99);
@@ -27,7 +29,9 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(argv[2])(num1, num2));
+	res = f(num1, num2);
+
+	printf("%d\n", res);
 
 	return (0);
 }
