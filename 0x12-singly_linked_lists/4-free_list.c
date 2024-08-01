f898a38d11f32a233f3617246ffd377fba3d05dc
LinMihigo <linmihigo@gmail.com>
2024-08-01 14:55:26 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+include "lists.h"
+/**
+* free_list - frees the list
+* @head: pointer to the head node
+* Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *list; 
+
+	while (head != NULL)
+	{
+		list = head->next;
+		free(list->str)
+		free(head);
+		head = list;
+	}
+}
