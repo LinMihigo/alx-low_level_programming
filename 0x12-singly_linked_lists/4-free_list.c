b0a72173aa97efa58b0684e703079b3f1993334f
LinMihigo <linmihigo@gmail.com>
2024-08-02 07:16:19 -0700

    7th fix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,7 +9,7 @@ void free_list(list_t *head)
 	list_t *current = head;
 	list_t *next;
 
-	while (current != NULL && current->next != NULL)
+	while (current != NULL)
 	{
 		next = current->next;
 		free(current->str);
