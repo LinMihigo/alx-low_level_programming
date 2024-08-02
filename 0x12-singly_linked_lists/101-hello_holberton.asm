173a530446f5153fb61c0aff1c66ce5a524512d3
LinMihigo <linmihigo@gmail.com>
2024-08-02 07:58:51 -0700

    First time writing assemby

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section	.text
+    global main
+    extern printf
+
+main:
+    ;Call printf
+    mov rdi, format
+    mov rsi, message
+    xor rax, rax        ;   Clear RAX to indicate no floating pt args
+    call printf
+
+    ;Exit the program
+    mov rax, 60     ;system call for exit
+    xor rdi, rdi    ;exit code 0
+    syscall
+
+section .data
+    message db 'Hello, Holberton', 0
+    format db '%s', 10, 0   ; %s for string, 10 for newline, 0 for \0
