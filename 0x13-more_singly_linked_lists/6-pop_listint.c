a94485dd4b1f7cddb73eb4ab426e261c825775a8
LinMihigo <linmihigo@gmail.com>
2024-08-06 12:57:36 -0700

    6-pop_listint.c (2nd fix)

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,12 +9,12 @@ int pop_listint(listint_t **head)
 	int n;
 	listint_t *temp;
 
-	if (*head == NULL || head == NULL)
+	if ((*head) == NULL || head == NULL)
 		return (0);
 
-	n = (*head)->n;
 	temp = *head;
 	*head = (*head)->next;
+	n = temp->n;
 
 	return (n);
 }
