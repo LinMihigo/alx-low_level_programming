1b641bd5b8dbf5675c81a69a1618560624da9b0c
LinMihigo <linmihigo@gmail.com>
2024-08-06 10:46:39 -0700

    Revert "5-free_listint2.c (2nd fix)"
    This reverts commit 985a45366d78f8537a63dfbd28a96e91e656bdf3.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,23 +6,17 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *current, *temp;
+	listint_t *current;
 
-	if (head != NULL)
+	if (!head)
+		return;
+
+	while ((*head))
 	{
-		/*set head addr to current*/
-		current = *head;
-
-		/*iterate through the whole list*/
-		/*while setting the current node to temp*/
-		while ((temp = current) != NULL)
-		{
-			/*set next node to curretnt*/
-			current = current->next;
-			/*free temp, that is the current node*/
-			free(temp);
-		}
-
-		*head = NULL;
+		current = (*head)->next;
+		free((*head));
+		(*head) = current;
 	}
+
+	(*head) = NULL;
 }
