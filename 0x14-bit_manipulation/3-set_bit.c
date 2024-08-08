e5089f6e912fa298a16886afe46ea354a5a705af
LinMihigo <linmihigo@gmail.com>
2024-08-08 14:33:09 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_bit - set value of a bit to 1 at a given index
+ * @n: pointer 
+ * @index: index
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= MAX_BITS_ULONG_INT)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,9 +5,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define MAX_BITS_ULONG_INT 64;
+
 void _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
