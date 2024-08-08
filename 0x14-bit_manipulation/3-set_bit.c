526c682dbf6627859031d9a46cd16f7691afe88a
LinMihigo <linmihigo@gmail.com>
2024-08-08 14:52:36 -0700

    3-set_bit.c (fix comp error)

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,8 +7,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 64)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	return ((n >> index) & 1);
+	*n |= (1UL << index);
+
+	return (1);
 }
