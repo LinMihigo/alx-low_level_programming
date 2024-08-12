b91ed7cab0423c1c98c13904dac884b2e612dce9
LinMihigo <linmihigo@gmail.com>
2024-08-12 13:06:00 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "main.h"
+/**
+* _strlen - str len
+* @str: pointer to str
+* Return: len
+*/
+int _strlen(char *str)
+{
+	int n;
+
+	while (*str++ != '\0')
+		n++;
+
+	return (n);
+}
+/**
+ * read_textfile - read a text file and prints it to STDOUT
+ * @filename: file name to create
+ * @text_content: string to write to file
+ * Return: 1 on success, -1 on failure
+ */
+ int create_file(const char *filename, char *text_content)
+ {
+	int c, w, len;
+
+	if (!filename)
+		return (-1);
+
+	c = creat(filename, S_IRUSR | S_IWUSR);
+	if (text_content)
+		len = _strlen(text_content);
+
+	w = write(c, text_content, len);
+
+	if (c == -1 || w == -1)
+		return (-1);
+
+	close(c);
+
+	return (1);
+ }
