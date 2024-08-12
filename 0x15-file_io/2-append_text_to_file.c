1b907ced1f508a73cc6674c87cc4a7570ef900c9
LinMihigo <linmihigo@gmail.com>
2024-08-12 14:08:16 -0700

    2-append_text_to_file.c (6th fix)

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,17 +12,19 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
+	o = open(filename, O_WRONLY | O_APPEND);
+	if (o == -1)
+		return (-1);
+
 	if (!text_content)
 	{
-		for (len = 0; text_content[len];)
+		while (text_content[len] != '\0';)
 			len++;
+
+		w = write(o, text_content, len);
+		if (w == -1)
+			return (-1);
 	}
-
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
 
 	close(o);
 
