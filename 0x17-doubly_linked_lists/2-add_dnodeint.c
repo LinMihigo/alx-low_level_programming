30057ccd8db3c2e433de3bd0a3fba4fb3301c211
LinMihigo <linmihigo@gmail.com>
2024-09-19 21:13:17 +0200

    2-add_dnodeint.c (1st fix)

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,26 +1,26 @@
 #include "lists.h"
 /**
-* add_dnodeint - add a new node at the beginning of list
-* @head: pointer to the head of the list
-* @n: num to add to the list
-* Return: address of new element as head of the list
-*/
+ * add_dnodeint - add a new node at the beginning of list
+ * @head: pointer to the head of the list
+ * @n: num to add to the list
+ * Return: address of new element as head of the list
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-	dlistint_t *last;
-	dlistint_t *current = *head;
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
-		return NULL;
+		return (NULL);
+
 	new->n = n;
-	if (current)
-	{
-		current->prev = new;
-		new->next = current;
-		new->prev = last;
-	}
+	new->prev = NULL;
+	new->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = new;
+
+	*head = new;
 
 	return (new);
 }
